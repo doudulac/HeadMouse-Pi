214160e161c5422403c7f8b28a46515ca2de1980
doudulac <42091363+doudulac@users.noreply.github.com>
2020-10-10 16:35:23 -0400

    include requirements.txt in image

--- update_pigen.sh
+++ update_pigen.sh
@@ -2,5 +2,5 @@
 
 DIR=$(dirname "${BASH_SOURCE[0]}")
 PIGEN=~/src/pi-gen
-FILES="$DIR/head* $DIR/inst* $DIR/unins* $DIR/shape*"
-cp -v $FILES "$PIGEN"/headmousepi/01-install-hmpi/files/
+FILES="$DIR/head* $DIR/inst* $DIR/unins* $DIR/shape* $DIR/requirements.txt"
+cp -v $FILES "$PIGEN"/headmousepi/00-install-hmpi/files/
