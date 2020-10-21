63107cd1cc12306df359ee3737589c00d92c6269
doudulac <42091363+doudulac@users.noreply.github.com>
2020-10-20 22:54:53 -0400

    use rsync to deploy updates

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,8 @@ if [ x"$1" = "x" ]; then
   exit 1
 fi
 
-FILES="$DIR/head* $DIR/inst* $DIR/unins*"
+FILES="$DIR/head* $DIR/inst* $DIR/unins* $DIR/require* $DIR/shape*"
 
 ssh "$1" "[ ! -d /hmpi ] && sudo mkdir /hmpi && sudo chown pi:pi /hmpi"
 
-ssh "$1" test -e /hmpi/shape_predictor_68_face_landmarks.dat || FILES+=" $DIR/shape*"
-
-scp $FILES pi@"$1":/hmpi/
+rsync -av --del -e "ssh -l pi" $FILES pi@"$1":/hmpi/
